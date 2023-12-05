8b12eb359ab7b1a2c4f4bda10afd3d8875d01a9c
gregelious <90730434+gregelious@users.noreply.github.com>
2023-12-05 02:25:27 -0500

    added coin

---
+++ pacman/Assets/Scripts/Coin.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //function for collision
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") // if the player touches it
+        {
+            transform.position = new Vector2(transform.position.x + 100, transform.position.y + 100); // it goes far away, looks like it's gone
+           // Destroy(gameObject); // self destructs
+        }
+
+    }
+}
