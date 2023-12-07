bdf9e2a4d716cf2e7913228845fff0486b049927
gregelious <90730434+gregelious@users.noreply.github.com>
2023-12-06 21:43:47 -0500

    player can collect coins now

--- pacman/Assets/Scripts/Coin.cs
+++ pacman/Assets/Scripts/Coin.cs
@@ -21,8 +21,9 @@ public class Coin : MonoBehaviour
     {
         if (other.tag == "Player") // if the player touches it
         {
-            transform.position = new Vector2(transform.position.x + 100, transform.position.y + 100); // it goes far away, looks like it's gone
-           // Destroy(gameObject); // self destructs
+            Debug.Log("touched");
+            //transform.position = new Vector2(transform.position.x + 100, transform.position.y + 100); // it goes far away, looks like it's gone
+            Destroy(gameObject); // self destructs
         }
 
     }
